29bdcc23f360363a735490f2f88f69c0932a00c7
Morgan Adamiec <morgan.adamiec@ably.com>
2022-06-14 00:58:20 +0100

    Enforce code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+  },
+};
